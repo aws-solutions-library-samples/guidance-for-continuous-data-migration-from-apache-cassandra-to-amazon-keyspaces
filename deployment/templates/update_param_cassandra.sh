deb33e3ce1f88080659ac8a757fadf45aa3d3f7c
naveenkantamneni <navi.k19@gmail.com>
2024-07-25 15:53:22 -0700

    cfn and readme changes

--- deployment/templates/update_param_cassandra.sh
+++ deployment/templates/update_param_cassandra.sh
@@ -6,7 +6,7 @@ subnet_three=$(cat stack_resources_output | grep PrivateSubnetThree | awk '{prin
 msk_sg=$(cat stack_resources_output | grep MSKSecurityGroupID | awk '{print $2}')
 
 connect_arn=$(aws kafkaconnect --region $AWS_REGION list-custom-plugins | grep customPluginArn | grep kafka-keyspaces-sink-plugin | awk '{print $2}')
-msk_arn=$(cat stack_resources_output | grep BlogMSKClusterArn | awk '{print $2}')
+msk_arn=$(cat stack_resources_output | grep SolMSKClusterArn | awk '{print $2}')
 msk_bootstrap_brokers=$(aws kafka get-bootstrap-brokers --cluster-arn $msk_arn | grep 'BootstrapBrokerString'| awk '{print $2}'| head -n 1)
 msk_role_arn=$(cat stack_resources_output | grep MSKconnectRoleID | awk '{print $2}')
 
